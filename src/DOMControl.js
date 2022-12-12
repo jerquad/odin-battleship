b7cff6ae251ea17625b62f37bc030b1d70b0ce18
Jimmy <jimmyquadros@hotmail.com>
2022-12-11 20:03:54 -0500

    add toggle x and y to setPlayer

--- src/DOMControl.js
+++ src/DOMControl.js
@@ -27,12 +27,9 @@ export class SetPlayer {
     constructor(size, pieces) {
         this.SIZE = size;
         this.container = makeElement('div', { id: 'set-container' });
-        this.container.appendChild(buildGrid(size, 'set-grid'));
+        this.container.appendChild(buildGrid(size));
+        this.container.appendChild(this.buildToggleBar());
         this.container.appendChild(this.makeTray(pieces));
-        // handle conflicts with mouse leaving window while in drag
-        document.querySelector('#main-container').addEventListener('mouseleave', () => {
-            return this.dragEndHandler();
-        });
         // container for drag information
         this.dragEvent = {
             dragItem: null,
@@ -41,89 +38,40 @@ export class SetPlayer {
             dragOver: null,
             dragGroup: []
         };
-        // bound function to handle dragging motion
-        this.dragMove = function(e) {
-            e = e || window.event;
-            e.preventDefault();
-            const icon = this.dragEvent.dragItem;
-            icon.style.top = (e.clientY - this.dragEvent.yOff) + 'px';
-            icon.style.left = (e.clientX - this.dragEvent.xOff) + 'px';
-
-            icon.style.visibility = 'hidden';
-            let below = document.elementFromPoint(e.clientX, e.clientY);
-            icon.style.visibility = 'visible';
-
-            if (below.classList.contains('selectable') 
-                && this.dragEvent.dragOver != below) {
-                    this.clearHover();
-                    this.setHover(below, Number(icon.dataset.left), Number(icon.dataset.right));
-            } else if (!below.classList.contains('selectable')) {
-                this.clearHover();
-            }
-        }
         this.dragMoveHandler = this.dragMove.bind(this);
-        // bound function to clear dragged actions
-        this.dragEnd = function (e) {
-            e = e || window.event;
-            e.preventDefault();
-            if (this.dragEvent.dragItem) { this.dragEvent.dragItem.remove() };
-            this.clearHover();
-            document.querySelector('#main-container').removeEventListener('mousemove', this.dragMoveHandler, true);
-            document.querySelector('#main-container').removeEventListener('mouseup', this.dragEndHandler, true);
-        }
         this.dragEndHandler = this.dragEnd.bind(this);
+        // handle conflicts with mouse leaving window while in drag
+        document.querySelector('#main-container').addEventListener('mouseleave', () => {
+            return this.dragEndHandler();
+        });
     }
 
-    // Handle adding hover event
-    setHover(cell, left, right) {  
-        cell.classList.add('hover');
-        this.dragEvent.dragOver = cell;
-        this.dragEvent.dragGroup.push(cell);
-        this.hoverSiblings(cell, left, true);
-        this.hoverSiblings(cell, right, false);                        
-    }
-
-    // Helper function to highlight the correct neighboring siblings on hover
-    hoverSiblings(root, numSib, left) {
-        let toAdd = root;
-        let index = Number(root.dataset.index);
-        const row = Math.floor(index / this.SIZE);
-        for (let i = 0; i < numSib; i++) {
-            const check = (left) ? ((index - (i + 1)) / this.SIZE) : ((index + (i + 1)) / this.SIZE);
-            if (row === Math.floor(check)) {
-                toAdd = (left) ? toAdd.previousSibling : toAdd.nextSibling;
-                toAdd.classList.add('hover'); 
-                this.dragEvent.dragGroup.push(toAdd);
+    buildToggleBar() {
+        const togglebar = makeElement('div', { id: 'toggle-bar' });
+        const toggleContainer = makeElement('div', { id: 'toggle-container' })
+        const toggleBody = makeElement('div', { id: 'toggle-body', 'data-y': 'false' });
+        const toggleText = makeElement('div', { id: 'toggle-text' }, 'X');
+        toggleBody.addEventListener('click', (e) => {
+            if (e.target.classList.contains('toggle-rotate')) {
+                e.target.setAttribute('data-y', 'false');
+                e.target.classList.remove('toggle-rotate');
+                toggleText.innerHTML = 'X';
+            } else {
+                e.target.setAttribute('data-y', 'true');
+                e.target.classList.add('toggle-rotate');
+                toggleText.innerHTML = 'Y';
             }
-        }
+        });
+        toggleContainer.appendChild(toggleBody);
+        toggleContainer.appendChild(toggleText);
+        togglebar.appendChild(toggleContainer)
+        return togglebar;
     }
 
-    clearHover() {
-        this.dragEvent.dragOver = null;
-        this.dragEvent.dragGroup.forEach(cell => cell.classList.remove('hover'));
-            this.dragEvent.dragGroup.length = 0;
-    }
-
-    // creates and appends dragged icon, sets drag bindings
-    dragStart(e) {
-        const icon = (e.target.classList.contains('icon-cell')) ? e.target.parentNode : e.target;
-        this.dragEvent.dragItem = this.makeDragIcon(Number(icon.dataset.size));
-        this.dragEvent.xOff = e.clientX;
-        this.dragEvent.yOff = e.clientY;
-        icon.appendChild(this.dragEvent.dragItem);
-
-        const xPos = e.clientX - this.dragEvent.dragItem.getBoundingClientRect().left;
-        const left = Math.floor(xPos / this.dragEvent.dragItem.firstChild.clientWidth);
-        const right = this.dragEvent.dragItem.childElementCount - left - 1;
-        this.dragEvent.dragItem.setAttribute('data-left', left);
-        this.dragEvent.dragItem.setAttribute('data-right', right);
-
-        document.querySelector('#main-container').addEventListener('mousemove', this.dragMoveHandler, true);
-        document.querySelector('#main-container').addEventListener('mouseup', this.dragEndHandler, true);
-    }
+    
 
     getContainer() { return this.container; }
-    getGrid() { return this.getContainer().querySelector('.set-grid'); }
+    getGrid() { return this.getContainer().querySelector('.grid-box'); }
     getTray() { return this.getContainer().querySelector('.set-tray'); }
 
     // append setplayer to main container, sets initial height of all icon cells
@@ -132,7 +80,7 @@ export class SetPlayer {
         document.querySelector('#main-container').appendChild(this.getContainer()); 
         this.setTrayIconSize()
         window.onresize = function() {
-            const height = document.querySelector('.set-grid').children.item(11).clientHeight;
+            const height = document.querySelector('.grid-box').children.item(11).clientHeight;
             document.querySelectorAll('.icon-cell').forEach(cell => {
                 cell.style.height = `${height}px`;
             })
@@ -156,7 +104,6 @@ export class SetPlayer {
     // container for icons
     makeTray(pieces) {
         const tray = makeElement('div', { class: 'set-tray' });
-        tray.setAttribute('draggable', false);
         for (let i = 0, j = Math.floor((pieces.length) / 2); j < pieces.length; i++, j++) {
             if (i < Math.floor(pieces.length) / 2) { 
                 tray.appendChild(this.makeTrayItem(pieces[i]));
@@ -169,7 +116,6 @@ export class SetPlayer {
     // create holder for icons
     makeTrayItem(piece) {
         const item = makeElement('div', { class: 'tray-item' })
-        item.setAttribute('draggable', false);
         item.appendChild(this.makePieceIcon(piece));
         return item;
     }
@@ -177,10 +123,8 @@ export class SetPlayer {
     // create icons
     makePieceIcon(size) {
         const icon = makeElement('div', { class: 'icon-piece' });
-        icon.setAttribute('draggable', false);
         for (let i = 0; i < size; i++) {
             const cell = makeElement('div', { class: 'icon-cell' });
-            cell.setAttribute('draggable', false);
             icon.appendChild(cell);
         }
         icon.dataset.size = size;
@@ -188,15 +132,123 @@ export class SetPlayer {
         return icon;
     }
 
-    makeDragIcon(size) {
+    makeDragIcon(size, vertical) {
         const dragIcon = this.makePieceIcon(Number(size));
         dragIcon.setAttribute('id', 'dragged');
+        if (vertical) { dragIcon.classList.add('drag-y') }
         [...dragIcon.children].forEach(child => {
             child.style.height = this.GetTrayIconSize()
             child.classList.add('drag-cell')
         });
-        return dragIcon;
-        
+        return dragIcon; 
+    }
+
+    // All drag/drop actions to follow
+
+    // creates and appends dragged icon, sets drag bindings
+    dragStart(e) {
+        const icon = (e.target.classList.contains('icon-cell')) ? e.target.parentNode : e.target;
+        const isY = (document.querySelector('#toggle-body').dataset.y === 'true');
+        this.dragEvent.dragItem = this.makeDragIcon(Number(icon.dataset.size), isY);
+        this.dragEvent.xOff = e.clientX;
+        this.dragEvent.yOff = e.clientY;
+        icon.appendChild(this.dragEvent.dragItem);
+
+        const xPos = e.clientX - this.dragEvent.dragItem.getBoundingClientRect().left;
+        const left = Math.floor(xPos / this.dragEvent.dragItem.firstChild.clientWidth);
+        const right = this.dragEvent.dragItem.childElementCount - left - 1;
+        this.dragEvent.dragItem.setAttribute('data-left', left);
+        this.dragEvent.dragItem.setAttribute('data-right', right);
+
+        if (this.dragEvent.dragItem.classList.contains('drag-y')) {
+            const newX = xPos - (this.dragEvent.dragItem.clientWidth / 2)
+            this.dragEvent.dragItem.style.left = `${newX}px`;
+            this.dragEvent.xOff -= newX;
+        }
+
+        document.querySelector('#main-container').addEventListener('mousemove', this.dragMoveHandler, true);
+        document.querySelector('#main-container').addEventListener('mouseup', this.dragEndHandler, true);
+    }
+
+    // bound function to handle dragging motion
+    dragMove(e) {
+        e = e || window.event;
+        e.preventDefault();
+        const icon = this.dragEvent.dragItem;
+        icon.style.top = (e.clientY - this.dragEvent.yOff) + 'px';
+        icon.style.left = (e.clientX - this.dragEvent.xOff) + 'px';
+
+        icon.style.visibility = 'hidden';
+        let below = document.elementFromPoint(e.clientX, e.clientY);
+        icon.style.visibility = 'visible';
+
+        if (below.classList.contains('selectable') 
+            && this.dragEvent.dragOver != below) {
+                this.clearHover();
+                if (this.dragEvent.dragItem.classList.contains('drag-y')) {
+                    this.setHoverY(below, this.dragEvent.dragItem.childElementCount);
+                } else {
+                    this.setHover(below, Number(icon.dataset.left), Number(icon.dataset.right));
+                }
+        } else if (!below.classList.contains('selectable')) {
+            this.clearHover();
+        }
+    }
+
+    // Function to clear dragged actions
+    dragEnd(e) {
+        e = e || window.event;
+        e.preventDefault();
+        if (this.dragEvent.dragItem) { this.dragEvent.dragItem.remove() };
+        this.clearHover();
+        document.querySelector('#main-container').removeEventListener('mousemove', this.dragMoveHandler, true);
+        document.querySelector('#main-container').removeEventListener('mouseup', this.dragEndHandler, true);
+    }
+
+    // Handle adding hover event
+    setHover(cell, left, right) {  
+        cell.classList.add('hover');
+        this.dragEvent.dragOver = cell;        
+        this.dragEvent.dragGroup.push(cell);
+        this.hoverSiblings(cell, left, true);
+        this.hoverSiblings(cell, right, false);                        
+    }
+
+    setHoverY(root, numSib) {
+        const grid = document.querySelector('.grid-box');
+        let index = Number(root.dataset.index);
+        let toAdd = root;
+        for (let i = 0; i < numSib; i++) {
+            if (toAdd) {
+                index = Number(toAdd.dataset.index);
+                this.dragEvent.dragGroup.push(toAdd);
+                toAdd.classList.add('hover');
+                toAdd = grid.children.item(adjustToIndex(index + this.SIZE, this.SIZE));
+            }
+        }
+
+    }
+
+    // Helper function to highlight the correct neighboring siblings on hover
+    hoverSiblings(root, numSib, left) {
+        let toAdd = root;
+        let index = Number(root.dataset.index);
+        const row = Math.floor(index / this.SIZE);
+        for (let i = 0; i < numSib; i++) {
+            const check = (left) ? ((index - (i + 1)) / this.SIZE) : ((index + (i + 1)) / this.SIZE);
+            if (row === Math.floor(check)) {
+                toAdd = (left) ? toAdd.previousSibling : toAdd.nextSibling;
+                toAdd.classList.add('hover'); 
+                this.dragEvent.dragGroup.push(toAdd);
+            }
+        }
+    }
+
+    // resets set hover actions
+    clearHover() {
+        this.dragEvent.dragOver = null;
+        this.dragEvent.dragGroup.forEach(cell => cell.classList.remove('hover'));
+        this.dragEvent.dragGroup.length = 0;
     }
 }
 
